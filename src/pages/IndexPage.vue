ff575956d67f90b24f891ac9aaa4af2571299a83
Khanh Khuu <khuu_giang_khanh@colpal.com>
2024-05-16 13:01:40 +0700

    Done int

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,16 +1,34 @@
 <template>
-  <q-page>
-    <q-card
-      class="q-mx-auto q-mb-lg"
-      flat
-    >
-      <q-card-section class="q-py-lg q-px-lg">
-        This is the Index Page
-      </q-card-section>
-    </q-card>
-  </q-page>
-</template>
-
-<script setup lang="ts">
-
-</script>
+    <q-page>
+      <q-card class="q-mx-auto q-mb-lg" flat>
+        <q-card-section class="q-py-lg q-px-lg">
+          <q-input v-model="thamSo1" label="Tham số 1" filled type="number" />
+  
+          <q-input v-model="thamSo2" label="Tham số 2" filled type="number" />
+  
+          <q-btn label="Tính tổng" @click="tinhTong" />
+  
+          <q-banner v-if="result" class="bg-primary text-white">
+            {{ result }}
+          </q-banner>
+        </q-card-section>
+      </q-card>
+    </q-page>
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from "vue";
+  
+  const thamSo1 = ref(0);
+  const thamSo2 = ref(0);
+  
+  const result = ref("");
+  
+  function tinhTong() {
+    const sum = thamSo1.value + thamSo2.value;
+    result.value = `Kết quả của phép toán: ${thamSo1.value} + ${thamSo2.value} là ${sum}`;
+    thamSo1.value = 0;
+    thamSo2.value = 0;
+  }
+  </script>
+  
